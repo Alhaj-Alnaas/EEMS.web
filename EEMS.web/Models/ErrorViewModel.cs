1a487c128915c01b7690044e6d9268cd78ede33c
النعاس <67058548+Alhaj-Alnaas@users.noreply.github.com>
2025-06-24 12:02:25 +0200

    Add project files.

---
+++ EEMS.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EEMS.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
