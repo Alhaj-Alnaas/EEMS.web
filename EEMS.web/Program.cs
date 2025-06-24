b77e8da5732194dc8e7cd127ca757ba8d394db34
النعاس <67058548+Alhaj-Alnaas@users.noreply.github.com>
2025-06-24 12:33:44 +0200

    test

--- EEMS.web/Program.cs
+++ EEMS.web/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// add this line by Alnaas to test
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
