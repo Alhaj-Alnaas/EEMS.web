2218b1c3aa7793361753f919b6fd5a259b91855f
النعاس <67058548+Alhaj-Alnaas@users.noreply.github.com>
2025-07-24 09:13:39 +0200

    Add files via upload

---
+++ Core/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using EEMS.Core.Interfaces.Repositories;
+using System.Threading.Tasks;
+
+namespace EEMS.Core.Interfaces.UnitOfWork
+{
+    public interface IUnitOfWork<T> where T : class
+    {
+        IGenericRepository <T> Entity { get; }
+        void Save();
+    }
+}
