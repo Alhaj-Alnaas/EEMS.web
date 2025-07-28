2204a8de02c7b4027f20179abe45710477ade273
AliALshikh <167680072+AliALshikh@users.noreply.github.com>
2025-07-28 11:33:42 +0200

    add

--- Core/Entities/equipMatiMovment.cs
+++ Core/Entities/equipMatiMovment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    public class equipMatiMovment
+    public class equipMatiMovment:Base
     {
         public string permitId { get; set; }
         public string description { get; set; }
