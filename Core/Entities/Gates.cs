0c06334ed5e2fe17d668db0108b343a80f4f9c83
AliALshikh <167680072+AliALshikh@users.noreply.github.com>
2025-07-28 11:28:59 +0200

    add

--- Core/Entities/Gates.cs
+++ Core/Entities/Gates.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    public class Gates
+    public class Gates:Base
     {
         public string no { get; set; }
         public string description  { get; set; }
--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcb5c66dee6e928884392786b9fa95198b63265e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e194be12b05f53bc2ac7cbf62a5d63284b6ee6b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
