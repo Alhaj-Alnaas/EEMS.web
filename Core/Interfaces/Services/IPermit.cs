2c8e128baa32e8ae1480b3e13fd816cdda3c724b
AliALshikh <167680072+AliALshikh@users.noreply.github.com>
2025-08-03 11:30:01 +0200

    pull

--- Core/Interfaces/Services/IPermit.cs
+++ Core/Interfaces/Services/IPermit.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities;
 
 namespace Core.Interfaces.Services
 {
-    internal interface IPermit
+    public interface IPermit
     {
+        void InsertPermit(Permit permit);
+        void UpdatePermit(Permit permit);
+        void DeletePermit(Permit permit);
+        Permit GetPermitById(string PermitId);
+        Permit GetPermitByType( string permitType);
+        List<Permit> GetUnApprovedPermit(string UserId);
+        List<Permit> GetPendingPermit( string UserId, string RespCode);
+        List<Permit> GetClosedPermit( string UserId);
+        List<Permit> GetAllPermit( string UserId);
+        List<Permit> SearchBy(List<Parameter> parameters);
+        string GeneratePermitSerialNumber( string permitType);
+
     }
 }
